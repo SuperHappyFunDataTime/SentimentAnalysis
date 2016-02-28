2c641a669a88020fef44e7a8d3cea69d106255fa
Ubuntu <ubuntu@ip-172-31-34-235.us-west-2.compute.internal>
2016-02-28 22:29:43 +0000

    add

---
+++ Data/sentiment_dict.py
@@ -0,0 +1,41 @@
+positive=[ 
+#    ':-)',':)',':o)',':]',':c)',':D','C:',   # JF, MS removed :3 because of confusion with times
+            u'\u263A',  # JF
+            u'\U0001F600',  # JF
+            u'\U0001F601',  # JF
+            u'\U0001F602',  # JF
+            u'\U0001F603',  # JF
+            u'\U0001F604',  # JF
+            u'\U0001F606',  # JF
+            u'\U0001F60A',  # JF
+            u'\U0001F60B',  # JF
+            u'\U0001F60D',  # JF
+            u'\U0001F60E',  # JF
+            u'\U0001F607',  # JF
+            u'\U0001F617',  # JF
+            u'\U0001F618',  # JF
+            u'\U0001F642',  # JF
+            u'\U0001F638',  # JF
+         ]
+negative =[
+#    ':-(',':(',':c',':[','D8','D;','D=','DX','v.v',  # JF
+            u"\u2639",  # JF
+            u'\U0001F613',  # JF
+            u'\U0001F614',  # JF
+            u'\U0001F615',  # JF
+            u'\U0001F616',  # JF
+            u'\U0001F621',  # JF
+            u'\U0001F622',  # JF
+            u'\U0001F623',  # JF
+            u'\U0001F624',  # JF
+            u'\U0001F625',  # JF
+            u'\U0001F626',  # JF
+            u'\U0001F627',  # JF
+            u'\U0001F628',  # JF
+            u'\U0001F629',  # JF
+            u'\U0001F62D',  # MS
+            u'\U0001F630',  # JF
+            u'\U0001F631',  # JF
+            u'\U0001F641',  # JF
+            ]
+
