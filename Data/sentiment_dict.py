aec91d763b2aa95852389b96d0723f9ca7c40322
jingfengmli@gmail.com <jingfengmli@gmail.com>
2016-03-21 18:07:57 +0000

    a

--- Data/sentiment_dict.py
+++ Data/sentiment_dict.py
@@ -1,5 +1,5 @@
 positive=[ 
-#    ':-)',':)',':o)',':]',':c)',':D','C:',   # JF, MS removed :3 because of confusion with times
+            ':-)',':)',':o)',':]',':c)',':D','C:',   # JF, MS removed :3 because of confusion with times
             u'\u263A',  # JF
             u'\U0001F600',  # JF
             u'\U0001F601',  # JF
@@ -18,7 +18,7 @@ positive=[
             u'\U0001F638',  # JF
          ]
 negative =[
-#    ':-(',':(',':c',':[','D8','D;','D=','DX','v.v',  # JF
+            ':-(',':(',':c',':[','D8','D;','D=','DX','v.v',  # JF
             u"\u2639",  # JF
             u'\U0001F613',  # JF
             u'\U0001F614',  # JF
